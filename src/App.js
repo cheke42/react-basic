5a4ac8a0fb742b54b4e5590a5f450ba0729ed18e
Cheke <cheke@example.com>
2021-09-28 13:45:22 -0300

    Propiedades

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import Componente from './components/Componente';
-
+import Propiedades from './components/Propiedades';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <section>
-        <Componente msg="Hola soy un componente funcional desde una prop"/>
-        <br></br>  
+        <section>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+        </section>
+        <section>
+          <Componente msg="Hola soy un componente funcional desde una prop"/>
+          <hr></hr>
+          <Propiedades 
+              cadena="Esto es una cadena de texto" 
+              numero={19} 
+              booleano={true}
+              arreglo={[1,2,3]}
+              objeto={{nombre:"Juan",email:"example@a.com"}}
+              funcion={(num) => num * num}
+              elementoReact={<i>Esto es un elemento React</i>}
+              componenteReact={<Componente msg="Esto es un segundo mensaje"/>} 
+          />
       </section>
+      </header>
+      
     </div>
   );
 }
---
+++ src/components/Propiedades.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Propiedades(props){
+    return (
+        <div>
+            <h2>{props.porDefecto}</h2>
+            <ul>
+                <li>{props.cadena}</li>
+                <li>{props.numero}</li>
+                <li>{props.booleano ? 'True' : 'False'}</li>
+                <li>{props.arreglo.join(", ")}</li>
+                <li>{JSON.stringify(props.objeto)}</li>
+                <li>{props.arreglo.map(props.funcion).join(", ")}</li>
+                <li>{props.elementoReact}</li>
+                <li>{props.componenteReact}</li>
+            </ul>
+        </div>
+    )
+}
+
+Propiedades.defaultProps = {
+    porDefecto: "Las Props"
+}
